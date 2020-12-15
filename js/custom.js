d751c083fba4e997834b9a32e0366a34b5b6a69d
Nicolas Chamorro <nichagiro@gmail.com>
2020-12-15 15:13:23 -0500

    adios erroes de consola

--- js/custom.js
+++ js/custom.js
@@ -34,52 +34,7 @@
 		}
 	});
 
-	/* ..............................................
-    Properties Filter
-    ................................................. */
-	var Container = $('.container');
-	Container.imagesLoaded(function () {
-		var portfolio = $('.properties-menu');
-		portfolio.on('click', 'button', function () {
-			$(this).addClass('active').siblings().removeClass('active');
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({
-				filter: filterValue
-			});
-		});
-		var $grid = $('.properties-list').isotope({
-			itemSelector: '.properties-grid'
-		});
-
-	});
-
-	/* ..............................................
-    Gallery
-    ................................................. */
 	
-	$(document).ready(function() {
-		$('.popup-gallery').magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			tLoading: 'Loading image #%curr%...',
-			mainClass: 'mfp-img-mobile',
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-				titleSrc: function(item) {
-					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-				}
-			}
-		});
-	});
-	
-	/* ..............................................
-    Scroll To Top
-    ................................................. */
 	
 	$(document).ready(function () {
 
